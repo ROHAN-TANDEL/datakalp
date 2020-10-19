686122438515eaf00f3e3f4a378999ddb11afed0
Rohan Tandel <67rohan@gmail.com>
2020-10-19 18:48:19 +0530

    first commit

---
+++ web/Products/Hygeine-AI/store.php
@@ -0,0 +1,11 @@
+<?php
+
+$output = 'details.txt';
+
+$success = file_put_contents($filename, $_REQUEST);
+
+
+everything was just a momment
+all theh time ust sleeping
+I wanna take leap and tell that 
+life screams when everythiing 
---
+++ web/Products/safehands/store.php
@@ -0,0 +1,5 @@
+<?php
+
+$output = 'details.txt';
+
+$success = file_put_contents($filename, $_REQUEST);
